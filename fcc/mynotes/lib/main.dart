98eec7e1451caf753990e216003c3dae992e991a
Emmanuel Guides <eguides@up.edu.ph>
2023-01-27 15:23:18 +0800

    Firebase auth, registration

--- fcc/mynotes/lib/main.dart
+++ fcc/mynotes/lib/main.dart
@@ -2,8 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
 void main() {
+  // initialize firebase
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -56,50 +59,77 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Center(child: Text('Register')) ,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            TextField( // email
-              controller: _email,
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                hintText: 'Enter Email',
-              ),
-            ),
-            TextField( // password
-              controller: _password,
-              obscureText: true,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration: const InputDecoration(
-                hintText: 'Enter Password',
-              ),
-            ),
-            TextButton(
-              onPressed: () async {
-                if (kDebugMode) {
-                  print("[PRESS] Balls");
-                }
-                final email = _email.text;
-                final password = _password.text;
-                // TODO initialize firebase 8:49:09
-                final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                    email: email, password: password
-                );
-                if (kDebugMode) {
-                  print(userCredential);
-                }
-              },
-              onLongPress: () {
-                if (kDebugMode) {
-                  print('[LONG PRESS] Ballserist');
-                }
-              },
-              child: const Text('Register'),
-            ),
-          ], // children
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
         ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState){
+            case ConnectionState.none:
+            // TODO: Handle this case.
+              return Text('oopsie1');
+              break;
+            case ConnectionState.waiting:
+            // TODO: Handle this case.
+              return Text('oopsie 2');
+              break;
+            case ConnectionState.active:
+            // TODO: Handle this case.
+              return Text('oopsie 3');
+              break;
+            case ConnectionState.done:
+              return Center(
+                child: Column(
+                  children: [
+                    TextField( // email
+                      controller: _email,
+                      autocorrect: false,
+                      enableSuggestions: false,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter Email',
+                      ),
+                    ),
+                    TextField( // password
+                      controller: _password,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter Password',
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        if (kDebugMode) {
+                          print("[PRESS] Balls");
+                        }
+                        final email = _email.text;
+                        final password = _password.text;
+                        // TODO initialize firebase 8:49:09
+                        // initialize firebase
+
+                        final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                            email: email, password: password
+                        );
+                        if (kDebugMode) {
+                          print(userCredential);
+                        }
+                      },
+                      onLongPress: () {
+                        if (kDebugMode) {
+                          print('[LONG PRESS] Ballserist');
+                        }
+                      },
+                      child: const Text('Register'),
+                    ),
+                  ], // children
+                ),
+              );
+            default:
+              return const Center(child: Text('Loading . . . '));
+          }
+        },
       ),
     );
   }
