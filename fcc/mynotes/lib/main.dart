8dbddd0be55c121f19b626f761caa48eb66a6b1c
Emmanuel Guides <eguides@up.edu.ph>
2023-01-28 18:27:42 +0800

    register_view.dart created

--- fcc/mynotes/lib/main.dart
+++ fcc/mynotes/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/register_view.dart';
+
 import 'firebase_options.dart';
 
 void main() {
@@ -22,43 +22,19 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const RegisterView(),
+      home: const HomePage(),
     );
   }
 }
 
-class RegisterView extends StatefulWidget {
-  const RegisterView({Key? key}) : super(key: key);
-
-  @override
-  State<RegisterView> createState() => _RegisterViewState();
-}
-
-class _RegisterViewState extends State<RegisterView> {
-  // variables
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState() {
-    // binding?
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('Register')) ,
+        title: const Center(child: Text('HOMEPAGE')) ,
       ),
       body: FutureBuilder(
         future: Firebase.initializeApp(
@@ -67,79 +43,16 @@ class _RegisterViewState extends State<RegisterView> {
         builder: (context, snapshot) {
           switch (snapshot.connectionState){
             case ConnectionState.none:
-            // TODO: Handle this case.
-              return Text('oopsie1');
+              return Text('None oh no');
               break;
             case ConnectionState.waiting:
-            // TODO: Handle this case.
-              return Text('oopsie 2');
+              return Text('Loading . . .');
               break;
             case ConnectionState.active:
-            // TODO: Handle this case.
               return Text('oopsie 3');
               break;
             case ConnectionState.done:
-              return Center(
-                child: Column(
-                  children: [
-                    TextField( // email
-                      controller: _email,
-                      autocorrect: false,
-                      enableSuggestions: false,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                        hintText: 'Enter Email',
-                      ),
-                    ),
-                    TextField( // password
-                      controller: _password,
-                      obscureText: true,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      decoration: const InputDecoration(
-                        hintText: 'Enter Password',
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () async {
-                        if (kDebugMode) {
-                          print("[PRESS] Balls");
-                        }
-                        final email = _email.text;
-                        final password = _password.text;
-                        // TODO Register Exceptions 9:39:57
-                        // initialize firebase
-                        try{
-                          final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                              email: email, password: password
-                          );
-                          if (kDebugMode) {
-                            print(userCredential);
-                          }
-                        } on FirebaseAuthException catch (e) {
-                          if(e.code == 'email-already-in-use' ){
-                            print('Email already in use!');
-                          } else if (e.code == 'weak-password'){
-                            print('Weak Password!');
-                          } else if (e.code == 'invalid-email') {
-                            print('Email inputted is invalid!');
-                          } else {
-                            print('Something wrong happened.');
-                            print(e);
-                          }
-                        }
-
-                      },
-                      onLongPress: () {
-                        if (kDebugMode) {
-                          print('[LONG PRESS] Ballserist');
-                        }
-                      },
-                      child: const Text('Register'),
-                    ),
-                  ], // children
-                ),
-              );
+              return const Center(child: Text('BRO DONEZA'));
             default:
               return const Center(child: Text('Loading . . . '));
           }
@@ -148,6 +61,7 @@ class _RegisterViewState extends State<RegisterView> {
     );
   }
 }
+
 
 
 
